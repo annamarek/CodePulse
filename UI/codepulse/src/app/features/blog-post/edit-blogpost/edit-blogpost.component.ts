a90fca0c1bb302179a5c1cd9958fad8438c8136b
annamarek <125365931+annamarek@users.noreply.github.com>
2025-06-13 00:36:46 +0300

    Display BlogPost Data in Edit Component

---
+++ UI/codepulse/src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+import { BlogPostService } from '../services/blog-post.service';
+import { BlogPost } from '../models/blog-post.model';
+import { CategoryService } from '../../category/services/category.service';
+import { Category } from '../../category/models/category.model';
+
+@Component({
+  selector: 'app-edit-blogpost',
+  templateUrl: './edit-blogpost.component.html',
+  styleUrls: ['./edit-blogpost.component.css']
+})
+export class EditBlogpostComponent implements OnInit, OnDestroy{
+  id: string | null = null;
+  routeSubscription?: Subscription;
+  model?: BlogPost;
+  categories$?: Observable<Category[]>;
+  selectedCategories?: string[];
+  constructor(private route: ActivatedRoute,
+    private blogPostService: BlogPostService,
+    private categoryService: CategoryService){
+
+  }
+
+  onFormSubmit(): void{
+
+  }
+
+  ngOnInit(): void {
+    this.categories$ = this.categoryService.getAllCategories();
+    this.routeSubscription = this.route.paramMap.subscribe({
+      next: (params) => {
+        this.id = params.get('id');
+        // Get BlogPost from API
+        if(this.id){
+          this.blogPostService.getBlogPostById(this.id)
+          .subscribe({
+            next: (response)=>{
+              this.model = response;
+              this.selectedCategories = response.categories.map(x=>x.id);
+            }
+          });
+        }
+
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.routeSubscription?.unsubscribe();
+  }
+
+}
